98f8c4b56c2d014cc0bb11e6c329abcad3012e03
2357gi <2357.gi@gmail.com>
2023-04-01 20:29:21 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HAND_RANGE_TABLE } from './data';
 
-function App() {
+function HandTable() {
+  return (
+    <table>
+      <tbody>
+        {HAND_RANGE_TABLE.map((row, rowIndex) => (
+          <tr key={`row-${rowIndex}`}>
+            {row.map((cell, cellIndex) => (
+              <td key={`cell-${rowIndex}-${cellIndex}`}>
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function MyApp() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>テキサスホールデムのハンド表</h1>
       </header>
+      <main>
+        <HandTable />
+      </main>
     </div>
   );
 }
 
-export default App;
+export default MyApp;
